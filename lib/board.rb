a7f250e9393b82f13a9435af8f0f39232c507c4c
Alan Bridger <alanrbridger@gmail.com>
2014-11-06 10:19:43 +0000

    Adds possible board method.

--- lib/board.rb
+++ lib/board.rb
@@ -1,13 +1,51 @@
+require_relative './cell.rb'
+require_relative './water.rb'
+
 class Board
 
-  def initialize(width, height)
-    @width = width
-    @height = height
-    @size = @width * @height
-    @board = @size.times.map{Cell.new(Water.new)}
+  # def initialize(size)
+  #   @size = size
+  #   @board = @size.times.map{Cell.new(Water.new)}
+  #   @rows = @board.each_slice(10).map{|e| e}
+  #   @columns = @rows.transpose
+  # end
+
+  def initialize
+    @coordinates = []
+    @cells = []
+    @grid = {}
+    x_axis
+    y_axis
+    coordinates
+    make_grid
   end
 
-  def cell_count
-    cell_count = @board.count
+  def x_axis
+    @x_axis = ('a'..'j').to_a
   end
+  
+  def y_axis
+    @y_axis = (1..10).to_a
+  end
+
+  def coordinates
+    @coordinates = x_axis.map { |x| y_axis.map { |y| "#{x}#{y}".to_s }}
+  end
+
+  def make_grid
+    @grid = Hash[@coordinates.flatten.map {|coordinate, cell| [coordinate, Cell.new(Water.new)]}]
+  end
+
+  def grid
+    @grid
+  end
+
+  def rows
+    @rows = grid.each_slice(10).map{|e| e}
+  end
+
+  def columns
+    @columns = @rows.transpose
+  end
+
 end
