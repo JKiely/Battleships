3e80df56cdc72711b83c15cce33f5fd20470c686
JKiely <jjkiely@gmail.com>
2014-11-06 13:31:00 +0000

    Added Place down method

--- lib/player.rb
+++ lib/player.rb
@@ -2,7 +2,6 @@ class Player
 
   def initialize(board)
     @board = board
-
   end
 
   def fire_at(coordinate)
@@ -10,7 +9,30 @@ class Player
   end
 
   def place_ship(ship, coordinate, direction)
-    @board.place_ship(ship, coordinate, direction)
+  	col = coordinate.split('')[0]
+  	row = coordinate.split('')[1]
+  	case direction.downcase
+  	when down
+  		place_down(ship, col, row)
+  	when up
+  		place_up(ship, col, row)
+  	when left
+  		place_left(ship, col, row)
+  	when right
+  		place_right(ship, col, row)
+  	end
   end
+
+
+  def place_down(ship, col, row)
+  	positions = {(col + row).to_sym => ship}
+  	(ship.size - 1).times do |x|
+  		positions[(col.next! + row).to_sym] = ship
+  	end
+  	@board.place_ship(positions)
+  end
+
+
+  
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -7,16 +7,21 @@ describe Player do
   let(:board) {double :board}
   let(:player) { Player.new(board)}
   let(:cell) {double :cell}
-  let(:ship_name) {double :ship_name}
+  let(:ship_name) {double :ship_name, size: 3}
 
     it 'should be able to declare cell it is shooting at' do
       expect(board).to receive(:fire_at)
       player.fire_at('A9')
     end
 
-    it "should be able to place ship on board" do
-      expect(board).to receive(:place_ship).with(ship_name, 'A9', 'South')
-      player.place_ship(ship_name, 'A9', 'South')
+    it "should be able to place ship on board pointing south" do
+      expect(board).to receive(:place_ship).with({:A1 => ship_name, :B1 => ship_name, :C1 => ship_name})
+      player.place_down(ship_name, 'A', '1')
+    end
+
+    it "should be able to place ship on board pointing south" do
+      expect(board).to receive(:place_ship).with({:A1 => ship_name, :A2 => ship_name, :A3 => ship_name})
+      player.place_right(ship_name, 'A', '1')
     end
 
 
